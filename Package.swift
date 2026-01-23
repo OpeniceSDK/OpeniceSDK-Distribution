3e1b08519a7cb129a9929f9984028ebe621e816f
JamieYee <yishangfei.me@gmail.com>
2026-01-23 16:04:50 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .binaryTarget(
             name: "OpeniceSDK",
             url: "https://github.com/OpeniceSDK/OpeniceSDK-Distribution/releases/download/v1.0.0/OpeniceSDK-v1.0.0.xcframework.zip",
-            checksum: "6c5e32cf4a4d9b504826e73d04e3b45bc3772f02108464fff0f0d9b1bd0820fc"
+            checksum: "cc7db6ea14f69e9ac9bafc545749ca766e9b53be303ede0287eabdbb30acea4a"
         )
     ]
 )
